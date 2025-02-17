5000fd88da4691cb81cc365b674ae8f19cf66021
Martin Zeleny <mzeleny@redhat.com>
2025-02-17 14:00:25 +0100

    01: ukol - rozsireni testu

--- 01/test_promitani.py
+++ 01/test_promitani.py
@@ -2,8 +2,18 @@
 
 from promitani import prevod
 
-def test_prevod():
+
+def test_ukazka():
     vstup = [136, 105, 82]
     vystup = ['2:16', '1:45', '1:22']
+    assert prevod(vstup) == vystup
 
+
+def test_prazdny():
+    assert prevod([]) == []
+
+
+def test_specialni():
+    vstup = [1, 61]
+    vystup = ['0:01', '1:01']
     assert prevod(vstup) == vystup
