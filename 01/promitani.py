bed72a4b668e9e48e8bbaf7927025633f9e39495
Veru Třísková <triskovaver@gmail.com>
2025-02-22 14:13:41 +0100

    test gitu

--- 01/promitani.py
+++ 01/promitani.py
@@ -1,4 +1,6 @@
 # https://kodim.cz/czechitas/uvod-do-progr-2/bonusy/cykly-2/list-comprehension/promitani
 
 def prevod(vstup):
-    ...
+    result = []
+    result = [str(round(delka / 60)) + ":" + str(delka % 60) for delka in vstup]
+
