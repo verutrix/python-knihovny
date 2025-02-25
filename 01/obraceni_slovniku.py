2cb5691ace3c568229542cd183f2f720da27d7fa
Veru Třísková <triskovaver@gmail.com>
2025-02-25 19:03:15 +0100

    opravena lekce 1 podle komentaru

--- 01/obraceni_slovniku.py
+++ 01/obraceni_slovniku.py
@@ -2,7 +2,7 @@
 # Použij dict comprehension
 
 def obrat_slovnik(slovnik):
-    return {slovnik[k]: k for k in slovnik}
+    return {v: k for k, v in slovnik.items()}
 
 
 # Napoveda:
--- 01/promitani.py
+++ 01/promitani.py
@@ -1,6 +1,5 @@
 # https://kodim.cz/czechitas/uvod-do-progr-2/bonusy/cykly-2/list-comprehension/promitani
-import math
 
 def prevod(vstup):
-    return [f"{math.floor(delka / 60)}:{delka % 60:02d}" for delka in vstup]
+    return [f"{(delka // 60)}:{delka % 60:02d}" for delka in vstup]
     
