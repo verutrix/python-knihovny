9a604ef6f8629eac69d0c736977eddd844c87aef
Veru Třísková <triskovaver@gmail.com>
2025-02-25 18:53:47 +0100

    opraveno zaokrouhlovani

--- 01/mnozina_zaokrouhlenych.py
+++ 01/mnozina_zaokrouhlenych.py
@@ -1,8 +1,4 @@
 # Napiš funkci, která vrátí ze iterovatelného vstupu množinu zaokrouhlených hodnot
 
-vstup = {1, 1.1, 1.2, 1.9, 2, 2.4, 10, 10.9}
-
 def mnozina_zaokrouhlenych(vstup):
     return {round(hodnota) for hodnota in vstup}
-
-print(mnozina_zaokrouhlenych(vstup))
