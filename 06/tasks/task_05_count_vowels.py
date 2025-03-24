e821afeab52909544fb94e8e8d60df4835b0c98d
Kristýna Kedajová <kristynakedajova@gmail.com>
2025-03-24 20:17:37 +0100

    05 Pocitani samohlasek

--- 06/tasks/task_05_count_vowels.py
+++ 06/tasks/task_05_count_vowels.py
@@ -4,4 +4,10 @@ def count_vowels(s: str) -> int:
     """
     Vrátí počet samohlásek v řetězci.
     """
-
+    samohlasky = "AEIOU"
+    s = s.upper()
+    pocet_vyskytu = 0
+    for pismeno in s:
+        if pismeno in samohlasky:
+            pocet_vyskytu += 1
+    return pocet_vyskytu
