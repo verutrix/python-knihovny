e40544fb99cb3ed14e831aba6e57cd635127c218
MartinMyska <martin.mysska@gmail.com>
2025-03-24 19:56:31 +0100

    14: Init remove zeros file

--- 06/tasks/task_14_remove_zeros.py
+++ 06/tasks/task_14_remove_zeros.py
@@ -5,3 +5,11 @@ def remove_zeros(lst: list[int]) -> list[int]:
     Vrátí seznam bez nulových hodnot.
     """
 
+    no_zero_list = lst.copy()
+
+    while True:
+        if 0 in no_zero_list:
+            no_zero_list.remove(0)
+        else:
+            break
+    return no_zero_list
