65f58798f40a7749bf629221ba6af8cf4327d666
Dragca Kvasnickova <kvasnickova@kvadr.eu>
2025-03-24 19:58:32 +0100

    09 seznam bez duplicitních prvků

--- 06/tasks/task_09_unique_elements.py
+++ 06/tasks/task_09_unique_elements.py
@@ -4,4 +4,9 @@ def unique_elements(lst: list[int]) -> list[int]:
     """
     Vrátí seznam bez duplicitních prvků, zachová původní pořadí.
     """
+    new = []
+    for element in lst:
+        if element not in new:
+            new.append(element)
 
+    return new
