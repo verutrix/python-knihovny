b8d2778ce827153b6e845f186e3e2be970371af0
MartinMyska <martin.mysska@gmail.com>
2025-03-24 20:29:02 +0100

    22: Fibonacci file

--- 06/tasks/task_22_fibonacci.py
+++ 06/tasks/task_22_fibonacci.py
@@ -5,3 +5,14 @@ def fibonacci(n: int) -> list[int]:
     Vrátí prvních n členů Fibonacciho posloupnosti.
     """
 
+    fibonacci_list = []
+
+    for num in range(0, n):
+        if len(fibonacci_list) < 2:
+            fibonacci_list.append(num)
+        else:
+            num_1 = fibonacci_list[-1]
+            num_2 = fibonacci_list[-2]
+            fibonacci_list.append(num_1 + num_2)
+
+    return fibonacci_list
