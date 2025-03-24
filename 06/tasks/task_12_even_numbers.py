80651796f548cdfd6272e45a0bf45ed32acbc019
Dagmar Vodáková <d.vodakova@seznam.cz>
2025-03-24 20:23:02 +0100

    12 sudá čísla

--- 06/tasks/task_12_even_numbers.py
+++ 06/tasks/task_12_even_numbers.py
@@ -4,4 +4,8 @@ def even_numbers(lst: list[int]) -> list[int]:
     """
     Vrátí seznam pouze sudých čísel.
     """
-
+    suda_cisla = []
+    for cislo in lst:
+        if cislo % 2 == 0:
+            suda_cisla.append(cislo)
+    return suda_cisla
