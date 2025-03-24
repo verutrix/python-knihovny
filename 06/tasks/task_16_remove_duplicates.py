8bcee396ce6e5c7f9218c789b422b6a1ea4a05e0
Jana D <drazkova.j@gmail.com>
2025-03-24 19:54:21 +0100

    16 Odstranění duplikátu v seznamu celých čísel

--- 06/tasks/task_16_remove_duplicates.py
+++ 06/tasks/task_16_remove_duplicates.py
@@ -4,4 +4,8 @@ def remove_duplicates(lst: list[int]) -> list[int]:
     """
     Vrátí nový seznam bez duplicitních hodnot, zachová původní pořadí.
     """
-
+    new_list = []
+    for i in lst:
+        if i not in new_list:
+            new_list.append(i)
+    return new_list
