cca95e941a9034cba241727970be8b5e42333af8
Dagmar Vodáková <d.vodakova@seznam.cz>
2025-03-25 17:35:12 +0100

    40 Nejčastější výskyt

--- 06/tasks/task_40_most_frequent.py
+++ 06/tasks/task_40_most_frequent.py
@@ -4,4 +4,10 @@ def most_frequent(items: list) -> any:
     """
     Vrátí nejčastěji se vyskytující prvek v seznamu.
     """
-
+    highest_number = 0
+    for item in items:
+        number = items.count(item)
+        if number > highest_number:
+            highest_number = number
+            most_frequent_item = item
+    return most_frequent_item
