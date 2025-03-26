79ac815baa207b8bb6107706deabc421eacde77e
Dragca Kvasnickova <kvasnickova@kvadr.eu>
2025-03-26 08:53:33 +0100

    29 vrátí nejdelelší společný prefix

--- 06/tasks/task_29_longest_common_prefix.py
+++ 06/tasks/task_29_longest_common_prefix.py
@@ -5,3 +5,15 @@ def longest_common_prefix(strings: list[str]) -> str:
     Vrátí nejdelší společný prefix v seznamu řetězců.
     """
 
+    if not strings:  # in case when list of strings is empty
+        return ""
+
+# set first word in "strings" as prefix
+    prefix = strings[0]
+    for word in strings[1:]:
+        while not word.startswith(prefix):
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
+
+    return prefix
