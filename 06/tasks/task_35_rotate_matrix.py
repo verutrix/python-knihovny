ce195b974cea03347232428917eeba0ee97f85b2
Jana D <drazkova.j@gmail.com>
2025-03-29 19:34:13 +0100

    35 Rotate matrix

--- 06/tasks/task_35_rotate_matrix.py
+++ 06/tasks/task_35_rotate_matrix.py
@@ -4,4 +4,4 @@ def rotate_matrix(matrix: list[list[int]]) -> list[list[int]]:
     """
     Otočí čtvercovou matici o 90 stupňů ve směru hodinových ručiček.
     """
-
+    return [list(i) for i in list(zip(*matrix[::-1]))]
