f8780a5bb31120546f3b78850dc29e8894e64e29
Jimmy-42 <j.studeny42@gmail.com>
2025-03-29 09:51:41 +0100

    14 mazani nul z listu
    návrh na řešení, bez cyklu while

--- 06/tasks/task_14_remove_zeros.py
+++ 06/tasks/task_14_remove_zeros.py
@@ -1,15 +1,8 @@
 # task_14_remove_zeros.py
-
 def remove_zeros(lst: list[int]) -> list[int]:
     """
     Vrátí seznam bez nulových hodnot.
     """
-
-    no_zero_list = lst.copy()
-
-    while True:
-        if 0 in no_zero_list:
-            no_zero_list.remove(0)
-        else:
-            break
-    return no_zero_list
+    for _ in range(lst.count(0)):
+        lst.remove(0)
+    return lst
