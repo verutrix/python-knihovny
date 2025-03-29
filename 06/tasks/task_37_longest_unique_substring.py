d47a84e35b45ad1c8a5bc1380c036a40428fa88a
Jana D <drazkova.j@gmail.com>
2025-03-29 21:39:39 +0100

    37 longest unique substring with enumerate

--- 06/tasks/task_37_longest_unique_substring.py
+++ 06/tasks/task_37_longest_unique_substring.py
@@ -4,4 +4,18 @@ def longest_unique_substring(s: str) -> int:
     """
     Vrátí délku nejdelší podposloupnosti bez opakujících se znaků.
     """
+    substr = ''
+    longest_substring = 0
+    for (pos, letter) in enumerate(s):
+        if letter in substr:
+            len_substr = len(substr)
+            pos_duplicate = s.index(letter, pos - len_substr)
+            substr = s[pos_duplicate + 1: pos + 1]
+            if longest_substring < len_substr:
+                longest_substring = len_substr
+        else:
+            substr += letter
+    if longest_substring < len(substr):
+        longest_substring = len(substr)
 
+    return longest_substring
