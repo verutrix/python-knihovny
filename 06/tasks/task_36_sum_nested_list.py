1b59ece3612d4a43a375890318dd54caa6e084c4
Jana D <drazkova.j@gmail.com>
2025-03-30 13:51:37 +0200

    36 sum  nested list

--- 06/tasks/task_36_sum_nested_list.py
+++ 06/tasks/task_36_sum_nested_list.py
@@ -5,4 +5,10 @@ def sum_nested_list(nested: list) -> int:
     Sečte všechna čísla v (potenciálně zanořeném) seznamu.
     Nápověda: isinstance(), rekurze
     """
-
+    sum = 0
+    for i in nested:
+        if isinstance(i, list):
+            sum += sum_nested_list(i)
+        else:
+            sum += i
+    return sum
