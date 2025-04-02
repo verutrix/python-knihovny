8349a3d421af8cf486d53ebb292c70c19c4b4895
Dragca <49131083+Dragca@users.noreply.github.com>
2025-04-02 22:31:29 +0200

    19 vrátí True, pokud je řetězec palindrom (#41)

--- 06/tasks/task_19_is_palindrome.py
+++ 06/tasks/task_19_is_palindrome.py
@@ -4,5 +4,5 @@ def is_palindrome(s: str) -> bool:
     """
     Vrátí True, pokud je řetězec palindrom.
     """
-
-    return s == s[::-1]
+# checks the front and back halves of the word
+    return s[:(len(s) // 2)] == s[:-((len(s) // 2) + 1):-1]
