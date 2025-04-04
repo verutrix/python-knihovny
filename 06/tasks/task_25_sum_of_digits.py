95effa425ec7538e0d15ef8606cd410f4b90336e
Dagmar Vodáková <d.vodakova@seznam.cz>
2025-04-04 09:09:36 +0200

    25 součet číslic

--- 06/tasks/task_25_sum_of_digits.py
+++ 06/tasks/task_25_sum_of_digits.py
@@ -4,4 +4,7 @@ def sum_of_digits(n: int) -> int:
     """
     Vrátí součet číslic čísla.
     """
-
+    soucet = 0
+    for cislice in str(abs(n)):
+        soucet += int(cislice)
+    return soucet
