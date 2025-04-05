a51fb0ee4bb5a548ec92b379e562a28b41591c48
Jana D <drazkova.j@gmail.com>
2025-04-05 20:49:47 +0200

    27 Group anagrams

--- 06/tasks/task_27_group_anagrams.py
+++ 06/tasks/task_27_group_anagrams.py
@@ -4,4 +4,13 @@ def group_anagrams(words: list[str]) -> list[list[str]]:
     """
     Skupiny anagramů v seznamu slov. Seznamy musí být seřazené. Anagramy v seznamech také seřazené.
     """
-
+    words.sort()
+    anagrams = []
+    sorted_anagrams = []
+    for word in words:
+        if sorted(word) in sorted_anagrams:
+            anagrams[sorted_anagrams.index(sorted(word))].append(word)
+        else:
+            sorted_anagrams.append(sorted(word))
+            anagrams.append([word])
+    return anagrams
