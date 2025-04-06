8c8abc97918816987238fd8a2b2794165ab16f16
Jana D <drazkova.j@gmail.com>
2025-04-06 17:33:33 +0200

    33 Valid parentheses

--- 06/tasks/task_33_valid_parentheses.py
+++ 06/tasks/task_33_valid_parentheses.py
@@ -1,7 +1,18 @@
 # task_33_valid_parentheses.py
 
+def erase_brackets(s, par):
+    if par not in s:
+        return s
+    pos = s.index(par)
+    s = s[:pos] + s[pos + 2:]
+    return s
+
+
 def valid_parentheses(s: str) -> bool:
     """
     Zjistí, zda má řetězec validní závorky.
     """
-
+    while '()' in s or '[]' in s or '{}' in s:
+        for par in '()', '[]', '{}':
+            s = erase_brackets(s, par)
+    return s == ''
