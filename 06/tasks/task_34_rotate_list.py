91c114c10d78ddcf2966ab1368e04be7843a5dec
Jana D <drazkova.j@gmail.com>
2025-04-07 09:52:25 +0200

    34 Rotate list

--- 06/tasks/task_34_rotate_list.py
+++ 06/tasks/task_34_rotate_list.py
@@ -4,4 +4,7 @@ def rotate_list(lst: list[int], k: int) -> list[int]:
     """
     Posune prvky seznamu o k pozic doprava.
     """
-
+    pos = len(lst) - k
+    new_list = lst[pos:]
+    new_list.extend(lst[:pos])
+    return new_list
