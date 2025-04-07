1f191add02947bda82fc4a24d0a76a1a67260ff4
Dragca Kvasnickova <kvasnickova@kvadr.eu>
2025-04-07 08:49:48 +0200

    39 vrátí řetězec se samohláskami v opačném pořadí

--- 06/tasks/task_39_reverse_vowels.py
+++ 06/tasks/task_39_reverse_vowels.py
@@ -5,4 +5,22 @@ def reverse_vowels(s: str) -> str:
     Vrátí řetězec s přeházenými samohláskami v opačném pořadí.
     Ostatní znaky zůstávají na místě.
     """
+    vowels = "aeiou"  # vowels without y otherwise the test failed ("Python") == "Python"
 
+    if s == "":
+        return s
+
+    s_list = list(s)
+    s_vowels = []
+    s_vowels_position = []
+
+# Identify the vowels and their positions in the string.
+    for position, character in enumerate(s):
+        if character.lower() in vowels:
+            s_vowels.append(character)
+            s_vowels_position.append(position)
+# replacing rotated vowels to vowel positions
+    for char, pos in zip(reversed(s_vowels), s_vowels_position):
+        s_list[pos] = char
+
+    return ''.join(s_list)
